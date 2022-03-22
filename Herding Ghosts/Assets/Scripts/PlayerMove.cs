27642a86445235bb94b0f7fa611424147b345662
Adam Cvija <adam_sez_hi@msn.com>
2022-03-22 13:23:56 +0800

    Basic movement script
    Basic Movemnt in default scene

--- Herding Ghosts/Assets/Scripts/PlayerMove.cs
+++ Herding Ghosts/Assets/Scripts/PlayerMove.cs
@@ -1,18 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
+    [SerializeField]
+    private InputActions _inputActions;
+    [SerializeField]
+    private float speed;
+
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        _inputActions = new InputActions();
+    }
+
+    private void OnEnable()
+    {
+        _inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _inputActions.Disable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+
+
+    }
+
+    void Move()
+    {
+        _inputActions.Player.Move.ReadValue<Vector2>();
+
+        if (_inputActions.Player.Move.ReadValue<Vector2>().x < 0)
+        {
+            Debug.Log("Moving Left");
+        }
+        if (_inputActions.Player.Move.ReadValue<Vector2>().x > 0)
+        {
+            Debug.Log("Moving Right");
+        }
+        if (_inputActions.Player.Move.ReadValue<Vector2>().y < 0)
+        {
+            Debug.Log("Moving Down");
+        }
+        if (_inputActions.Player.Move.ReadValue<Vector2>().y < 0)
+        {
+            Debug.Log("Moving Up");
+        }
+
+        transform.position += new Vector3(_inputActions.Player.Move.ReadValue<Vector2>().x, _inputActions.Player.Move.ReadValue<Vector2>().y, 0) * speed * Time.deltaTime;
+
+
+
     }
 }
