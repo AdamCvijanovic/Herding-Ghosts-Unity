d45f969fe4291ea3f8ac42f106451106bfa5bfbe
Adam Cvija <adam_sez_hi@msn.com>
2022-03-23 17:10:03 +0800

    Preliminary movement
    Still got to fix cameras
    And Collisions

--- Herding Ghosts/Assets/Scripts/PlayerMove.cs
+++ Herding Ghosts/Assets/Scripts/PlayerMove.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
+
 
 public class PlayerMove : MonoBehaviour
 {
@@ -9,12 +11,20 @@ public class PlayerMove : MonoBehaviour
     private InputActions _inputActions;
     [SerializeField]
     private float speed;
+    [SerializeField]
+    private Vector2 _movementVector = new Vector2();
+    [SerializeField]
+    private Rigidbody2D _rb;
+
+    public Grid _grid;
+    public Tilemap _tilemap;
 
 
     // Start is called before the first frame update
     void Awake()
     {
         _inputActions = new InputActions();
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     private void OnEnable()
@@ -27,38 +37,68 @@ public class PlayerMove : MonoBehaviour
         _inputActions.Disable();
     }
 
+
     // Update is called once per frame
     void Update()
     {
         Move();
 
+        if (_inputActions.Player.Move.WasPressedThisFrame())
+        {
+            Vector3Int above = new Vector3Int(_grid.WorldToCell(transform.position).x, _grid.WorldToCell(transform.position).y + 1, 0);
 
+            //if (_tilemap.GetColliderType(above) == Tile.ColliderType.Sprite)
+            //{
+            //    Debug.Log("ABOVE");
+            //}
+
+            //Debug.Log("KEY PRESSED" +
+            //_grid.WorldToCell(transform.position).x + ", " +
+            //_grid.WorldToCell(transform.position).y + ", " +
+            //_tilemap.GetColliderType(above).ToString());
+
+            //_tilemap.GetColliderType(_grid.WorldToCell(transform.position));
+
+        }
     }
 
     void Move()
     {
-        _inputActions.Player.Move.ReadValue<Vector2>();
+        _movementVector = _inputActions.Player.Move.ReadValue<Vector2>();
 
         if (_inputActions.Player.Move.ReadValue<Vector2>().x < 0)
         {
-            Debug.Log("Moving Left");
+            //Debug.Log("Moving Left");
         }
         if (_inputActions.Player.Move.ReadValue<Vector2>().x > 0)
         {
-            Debug.Log("Moving Right");
+            //Debug.Log("Moving Right");
         }
         if (_inputActions.Player.Move.ReadValue<Vector2>().y < 0)
         {
-            Debug.Log("Moving Down");
+            //Debug.Log("Moving Down");
         }
         if (_inputActions.Player.Move.ReadValue<Vector2>().y < 0)
         {
-            Debug.Log("Moving Up");
+            //Debug.Log("Moving Up");
         }
 
-        transform.position += new Vector3(_inputActions.Player.Move.ReadValue<Vector2>().x, _inputActions.Player.Move.ReadValue<Vector2>().y, 0) * speed * Time.deltaTime;
+        Vector3Int above = new Vector3Int(_grid.WorldToCell(transform.position).x, _grid.WorldToCell(transform.position).y + 1, 0);
+
+        if (_tilemap.GetColliderType(above) == Tile.ColliderType.Sprite)
+        {
+            Debug.Log("ABOVE");
+        }
+        else
+        {
+            _rb.position += _movementVector * speed * Time.deltaTime;
+
+        }
+        //_rb.position += _movementVector * speed * Time.deltaTime;
 
 
 
     }
+
+
 }
