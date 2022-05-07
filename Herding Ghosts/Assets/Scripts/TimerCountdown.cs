d79581ef838afd153d1510a9663e7fac34cd0a74
Gameshredder <brendan.ian.jamieson@gmail.com>
2022-05-07 18:18:54 +0800

    Update TimerCountdown.cs
    Added a Time.timeScale = 1f; to the start of this to make sure the scene runs when loaded, was having issues when loading my test scene.

--- Herding Ghosts/Assets/Scripts/TimerCountdown.cs
+++ Herding Ghosts/Assets/Scripts/TimerCountdown.cs
@@ -10,11 +10,13 @@ public class TimerCountdown : MonoBehaviour
     public GameObject timer;
     public float currentTime = 60.0f;
     public float maxTime = 60.0f;
+    public GameObject _winMenu;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // added this to make sure the scene runs when loaded, had an issue where it wouldnt run on scene load
+        Time.timeScale = 1f;
     }
 
     // Update is called once per frame
@@ -25,6 +27,8 @@ public class TimerCountdown : MonoBehaviour
 
         if(currentTime <= 0)
         {
+            _winMenu.SetActive(true);
+            Time.timeScale = 0f;
             timer.SetActive(false);
         }
     }
