51396ba2f08d699e94b7cce96b7f103f5f30ed75
Gameshredder <brendan.ian.jamieson@gmail.com>
2022-08-07 18:46:28 +0800

    Update UITimer.cs
    Revamped timer to change colors at 10 sec to yellow and red at 5 sec

--- Herding Ghosts/Assets/Scripts/UI Scripts/UITimer.cs
+++ Herding Ghosts/Assets/Scripts/UI Scripts/UITimer.cs
@@ -5,18 +5,26 @@ using UnityEngine.UI;
 
 public class UITimer : MonoBehaviour
 {
-public float countdown = 10.0f;
+public float countdown = 15.0f;
 public float currentTime;
 public Image timerFill;
 public Color yellowColor;
 public Color redColor;
 public Animator shake;
-public GameObject menues;
+public GameObject menuBG;
 public GameObject losemenu;
+
+FMOD.Studio.EventInstance LastTenSeconds;
+
+    void Awake()
+    {
+        LastTenSeconds =  FMODUnity.RuntimeManager.CreateInstance("event:/LoseTimer");
+    }
 
     // Start is called before the first frame update
     void Start()
     {
+        
         currentTime = countdown;
         yellowColor.a = 1;
         redColor.a = 1;
@@ -27,6 +35,12 @@ public GameObject losemenu;
     {
         currentTime -= Time.deltaTime;
         timerFill.fillAmount = currentTime / countdown;
+
+        if(currentTime <= 10.0f && currentTime >= 9.9f)
+        {
+            LastTenSeconds.start();
+        }
+
         if(currentTime <= 10.0f && currentTime> 5.0f)
         {
             timerFill.color = yellowColor;
@@ -44,7 +58,7 @@ public GameObject losemenu;
         if(currentTime <= 0.0f)
         {
             Time.timeScale = 0f;
-            menues.SetActive(true);
+            menuBG.SetActive(true);
             losemenu.SetActive(true);
         }
 
