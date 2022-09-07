68a45ced120588d9f1100e6e0f5f182b38b2be2b
Adam Cvijanovic <adam_sez_hi@msn.com>
2022-09-07 22:43:24 +0800

    Level & Shopping integration

--- Herding Ghosts/Assets/Scripts/GhostScripts/GhostLogic.cs
+++ Herding Ghosts/Assets/Scripts/GhostScripts/GhostLogic.cs
@@ -94,7 +94,7 @@ public class GhostLogic : MonoBehaviour
         {
             oven = _destMngr.GetDestinationOfType(Destination.DestinationType.Oven).gameObject;
             cauldron = _destMngr.GetDestinationOfType(Destination.DestinationType.Cauldron).gameObject;
-            basement = _destMngr.GetDestinationOfType(Destination.DestinationType.Basement).gameObject;
+            //basement = _destMngr.GetDestinationOfType(Destination.DestinationType.Basement).gameObject;
         }
     }
 
