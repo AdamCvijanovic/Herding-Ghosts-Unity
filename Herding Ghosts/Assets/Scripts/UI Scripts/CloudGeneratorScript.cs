6ded52cce299753f47c32dc55c198434c4ba1de2
lilyleaf <98629617+lilyleaf@users.noreply.github.com>
2022-11-17 04:30:32 +0800

    tried to make cloud generator doesn't work
    added cloud script, cloud generator script to main menu

---
+++ Herding Ghosts/Assets/Scripts/UI Scripts/CloudGeneratorScript.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudGeneratorScript : MonoBehaviour
+{
+    [SerializeField]
+    GameObject [] clouds;
+
+    [SerializeField]
+    float spawnInterval;
+
+    [SerializeField]
+    GameObject endPoint;
+
+    Vector3 startPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+
+        Prewarm();
+
+        Invoke("AttemptSpawn", spawnInterval); //could be replaced with coroutine/delegate
+    }
+
+    void SpawnCloud(Vector3 startPos)
+    {
+        int randomIndex = UnityEngine.Random.Range(0, clouds.Length);
+        GameObject cloud = Instantiate(clouds[randomIndex]);
+
+        float startY = UnityEngine.Random.Range(startPos.y - 1.5f, startPos.y + 1.5f);
+        cloud.transform.position = new Vector3(startPos.x, startY, startPos.z);
+
+        //float scale = UnityEngine.Random.Range(0.8f, 1f);
+        //cloud.transform.localScale = new Vector2(scale, scale);
+
+        //float speed = UnityEngine.Random.Range(0.8f, 0.9f);
+        //cloud.GetComponent<CloudScript>().StartFloating(speed, endPoint.transform.position.x);
+    }
+
+    void AttemptSpawn()
+    {
+        SpawnCloud(startPos);
+        Invoke("AttemptSpawn", spawnInterval);  //loops function
+        //SpawnCloud(startPos);
+    }
+
+    void Prewarm()
+    {
+        for (int i = 0; i < 1; i++)
+        {
+            Vector3 spawnPos = startPos - (Vector3.right) * (i * 7);
+            SpawnCloud(spawnPos);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
