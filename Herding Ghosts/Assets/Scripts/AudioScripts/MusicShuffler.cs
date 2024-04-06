3d5dfb82802b20458a7a627070d9959c1d3442ef
albedoDeveloper <kyehorbury@msn.com>
2024-04-06 16:06:23 +0800

    Fixed shuffler and added help

--- Herding Ghosts/Assets/Scripts/AudioScripts/MusicShuffler.cs
+++ Herding Ghosts/Assets/Scripts/AudioScripts/MusicShuffler.cs
@@ -4,69 +4,40 @@ using UnityEngine;
 
 public class MusicShuffler : MonoBehaviour
 {
-    public AudioSource pattisserieSong1;
-    public AudioSource pattisserieSong2;
-    public AudioSource pattisserieSong3;
-    public AudioSource pattisserieSong4;
-    public AudioSource pattisserieSong5;
-    public AudioSource pattisserieSong6;
-
+    public AudioSource[] pattisserieSongList;
+    int currentTrack = 0;
+    private bool isGamePaused = false;
+   
     private bool isMusicPlaying;
 
-    private int randomNumber;
-    private int prevRandomNumber;
+    private int prevTrack;
 
     void Start()
     {
         isMusicPlaying = false;
     }
 
+    void OnApplicationPause(bool pauseStatus)
+    {
+        isGamePaused = pauseStatus;
+    }
+
     void Update()
     {
-        if (pattisserieSong1.isPlaying || pattisserieSong2.isPlaying || pattisserieSong3.isPlaying || pattisserieSong4.isPlaying || pattisserieSong5.isPlaying || pattisserieSong6.isPlaying)
-        {
-            isMusicPlaying = true;
-        }
-        else
-        {
+        if (!pattisserieSongList[currentTrack].isPlaying && !isGamePaused)
             isMusicPlaying = false;
-        }
+
 
         if (!isMusicPlaying)
-        {
+        {   
             do
             {
-                randomNumber = Random.Range(1, 7);
-            } while (randomNumber == prevRandomNumber);
+                currentTrack = Random.Range(0, pattisserieSongList.Length);
+            } while (currentTrack == prevTrack);
 
-            prevRandomNumber = randomNumber;
-
-            switch (randomNumber)
-            {
-                case 1:
-                pattisserieSong1.Play();
-                break;
-
-                case 2:
-                pattisserieSong2.Play();
-                break;
-
-                case 3:
-                pattisserieSong3.Play();
-                break;
-
-                case 4:
-                pattisserieSong4.Play();
-                break;
-
-                case 5:
-                pattisserieSong5.Play();
-                break;
-
-                case 6:
-                pattisserieSong6.Play();
-                break;
-            }
+            pattisserieSongList[currentTrack].Play();
+            prevTrack = currentTrack;
+            isMusicPlaying = true;
         }
     }
 }
